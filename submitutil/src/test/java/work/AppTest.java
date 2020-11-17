61290446c4f5799a17d1bfe7820d725ebca17b05
Wenbo Zhao (Beyondsoft) <v-zwenbo@microsoft.com>
2020-11-17 17:35:54 +0800

    work codes

--- submitutil/src/test/java/work/AppTest.java
+++ submitutil/src/test/java/work/AppTest.java
@@ -1,39 +1,57 @@
 package work;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Date;
-import java.util.LinkedHashMap;
 
-import org.apache.commons.lang3.time.DateFormatUtils;
-import org.apache.commons.lang3.time.DateUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClients;
 import org.junit.Test;
-
-import work.model.GoodModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.yaml.snakeyaml.reader.StreamReader;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue() {
-        Path currentPath = Paths.get("");
-        System.out.println(currentPath.toAbsolutePath().toString());
-    }
+  /**
+   * Rigorous Test :-)
+   */
+  private static final Logger LOG = LoggerFactory.getLogger(AppTest.class);
 
-    @Test
-    public void test2() {
-      HttpClient client = HttpClients.createDefault();
-      HttpGet get = new HttpGet("");
+  @Test
+  public void shouldAnswerWithTrue() {
+    Path currentPath = Paths.get("");
+    System.out.println(currentPath.toAbsolutePath().toString());
+  }
+
+  @Test
+  public void test2() {
+    HttpClient client = HttpClients.createDefault();
+    HttpGet get = new HttpGet("");
+  }
+
+  @Test
+  public void test3() {
+    String host = "127.0.0.1";
+    int port = 32998;
+    try {
+      ServerSocket server = new ServerSocket(port);
+      server.setSoTimeout(0);
+      Socket socket = server.accept();
+      BufferedInputStream reader = new BufferedInputStream(socket.getInputStream());
+      byte[] dataByteArr = null;
+      dataByteArr = reader.readAllBytes();
+      String s = new String(dataByteArr);
+      System.out.println(s);
+    } catch (IOException e) {
+      LOG.error("error,message :{}", e);
     }
+  }
 
 }
