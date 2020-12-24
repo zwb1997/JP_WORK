c2feae052a09b3f24587edbf46cd98341c997db8
zwb1997 <IIIIINNNCCC@outlook.com>
2020-12-24 23:13:46 +0800

    use yarn rebuild project and use electron-builder to create .exe

--- app/public/electron.js
+++ app/public/electron.js
@@ -4,18 +4,12 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    titleBarStyle: 'customButtonsOnHover',
-    show:false,// Avoid flicker
     webPreferences: {
       nodeIntegration: true
     }
   })
-  win.setMenu(null);
-  win.setTitle("app");
+
   win.loadFile('./build/index.html')
-  win.once('ready-to-show', () => {
-    win.show()
-  })
 }
 
 app.whenReady().then(createWindow)
